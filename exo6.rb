653a64d74f99e9600c06b705d710b7ec2fa7768d
Evajuliab <103074596+Evajuliab@users.noreply.github.com>
2022-04-12 22:19:12 +0200

    Add files via upload

---
+++ exo6.rb
@@ -0,0 +1,8 @@
+puts "donne un nombre"
+print ">"
+user_number = gets.chomp.to_i-1
+# (user_number).times do |i|
+#     puts "bonjour toi" 
+# end
+
+7.times {|bernqrd| puts "bonjour toi (repetition ##{i +1})" }
